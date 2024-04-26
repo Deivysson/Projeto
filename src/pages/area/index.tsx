7a293f05440093637ad0164fe3fd8e429e188ea7
Deivysson Gomes <105027772+Deivysson@users.noreply.github.com>
2024-04-26 12:00:08 -0300

    commit

--- src/pages/area/index.tsx
+++ src/pages/area/index.tsx
@@ -1,24 +1,40 @@
-
+import './area.module.css';
 import { useLocation } from "react-router-dom";
+import img from '../../../assets/imagens/micro.png'
 
 export function Area() {
     const location = useLocation();
     const queryParams = new URLSearchParams(location.search);
     const nom_paciente = queryParams.get('nom_paciente');
     const num_cpf = queryParams.get('num_cpf');
-    const des_endereco = queryParams.get('des_endereco');
     const des_email = queryParams.get('des_email');
 
     return (
         <div>
+        <div className="headerContainer" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
             <header>
                 <h1>Bem-vindo, {nom_paciente}!</h1>
                 <p>CPF: {num_cpf}</p>
-                <p>Endereço: { des_endereco } </p>
-                <p>Email: { des_email } </p>
+                <p>Email: {des_email}</p>
             </header>
-            
+            <img src={img} alt="Logo" className="logo" style={{ height: 'auto' }}/>
         </div>
-    );
-}
 
+        <table>
+    <thead>
+        <tr>
+            <th>Exames</th>
+            <th >Data de Inserção</th>
+        </tr>
+        <tr>
+            <th>Arquivo.pdf</th>
+            <th >23/04/2024</th>
+        </tr>
+    </thead>
+    <tbody>
+     
+    </tbody>
+</table>
+    </div>
+);
+}
