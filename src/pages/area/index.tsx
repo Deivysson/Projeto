68c851c528d9fc5105060a14bb1b8710fe32fe14
Deivysson Gomes <105027772+Deivysson@users.noreply.github.com>
2024-07-01 22:51:49 -0300

    commit

--- src/pages/area/index.tsx
+++ src/pages/area/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './area.module.css';
 import { useLocation } from "react-router-dom";
+import { FaEye, FaDownload } from 'react-icons/fa';
 import img from '../../../assets/imagens/micro.png';
 
 interface Exame {
@@ -29,14 +30,21 @@ export function Area() {
         }
 
         fetch(`http://localhost:3000/exames/arquivos?cod_paciente=${cod_paciente}`)
-            .then(response => response.json())
-            .then(data => setExames(data.exames))
-            .catch(error => console.error('Erro ao buscar exames:', error));
+        .then(response => response.json())
+        .then(data => {
+            const sortedExames = data.exames.sort((a: Exame, b: Exame) => {
+                return new Date(b.data_exame).getTime() - new Date(a.data_exame).getTime();
+            });
+            setExames(sortedExames);
+        })
+        .catch(error => console.error('Erro ao buscar exames:', error));
     }, [cod_paciente]);
 
     const handleDateClick = (date: string) => {
         setSelectedDate(selectedDate === date ? null : date);
     };
+
+    
 
     return (
         <div>
@@ -49,30 +57,34 @@ export function Area() {
                 <img src={img} alt="Logo" className="logo" style={{ height: 'auto' }} />
             </div>
 
-            <table>
+            <table className="exames-table">
                 <thead>
                     <tr>
-                        <th>Data do Exame</th>
+                        <th colSpan={2} className="exames-title">Exames realizados</th>
                     </tr>
                 </thead>
                 <tbody>
                     {exames.map((exame, index) => (
                         <React.Fragment key={index}>
-                            <tr onClick={() => handleDateClick(exame.data_exame)}>
-                                <td>{exame.data_exame}</td>
+                            <tr onClick={() => handleDateClick(exame.data_exame)} className={selectedDate === exame.data_exame ? 'selected-date' : ''}>
+                                <td>{exame.data_exame.split(' ')[0]}</td>
                             </tr>
                             {selectedDate === exame.data_exame && (
                                 <tr>
-                                    <td colSpan={1}>
+                                    <td colSpan={2}>
                                         <div className="exame-details">
-                                            <p><strong>Exame:</strong> {exame.exame}</p>
-                                            <p><strong>Médico:</strong> {exame.medico}</p>
-                                            <p>
-                                                <strong>Arquivo:</strong> 
-                                                <a href={`http://localhost:3000/upload/${exame.nome_arquivo}`} target="_blank" rel="noopener noreferrer">
-                                                    {exame.nome_arquivo}
+                                            <div className="exame-info">
+                                                <p><strong>Exame:</strong> {exame.exame}</p>
+                                                <p className="medico-info"><strong>Médico:</strong> {exame.medico}</p>
+                                            </div>
+                                            <div className="icon-buttons">
+                                                <a href={`http://localhost:3000/upload/${exame.nome_arquivo}`} target="_blank" rel="noopener noreferrer" className="icon-button">
+                                                    <FaEye size={25} title="Visualizar"/>
                                                 </a>
-                                            </p>
+                                                <a href={`http://localhost:3000/upload/${exame.nome_arquivo}`} target="_blank" rel="noopener noreferrer" download className="icon-button">
+                                                    <FaDownload size={25} title="Baixar"/>
+                                                </a>
+                                            </div>
                                         </div>
                                     </td>
                                 </tr>
